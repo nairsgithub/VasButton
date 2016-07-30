9df48186f20629ac1f372b42493648ad894660a0
AMIT NAIR <amitnair92@gmail.com>
2016-07-30 12:41:46 +0530

    Update VasButtonTest.py

--- VasButtonTest.py
+++ VasButtonTest.py
@@ -9,13 +9,16 @@ import json
 url="http://vastauine.com/VASAPI.php"
 
 #START MESSAGE
-MYUSR='YOUR-VASTAUINE-ID'
-MYPASS='YOUR-VASTAUINE-PASSWORD'
+MYUSR='change_USERNAME'
+MYPASS='change_PASSWORD'
 print("STARTED..", datetime.datetime.now().time())
 
-
-
 #FUNCTIONS
+def Write(text):
+    file = open("vasButtonLog.txt", "a")
+    file.write(text)
+    file.close()
+    
 def PiUpdate(pin,state):        
     import RPi.GPIO as GPIO
     GPIO.setwarnings(False)
@@ -24,36 +27,43 @@ def PiUpdate(pin,state):
     GPIO.output(pin,state)
     
 
+
 def Change(MYUSR,MYPASS,LastChange):
     try : 
         r=requests.post(url, data={'USERNAME' : MYUSR,'PASSWORD' : MYPASS, 'CHANGE' :'CHANGE'})
     except requests.ConnectionError as e:
-        return ['CHANGE#ERR',LastChange]
+        time.sleep(0.5)
+        return ['CHANGE*ERR',LastChange]
     else:
-        CHANGE=(json.loads(r.text.strip()))
-        if LastChange == CHANGE :
-            #print(LastChange,"returning NoChange",CHANGE)
-            return [True,LastChange]
-        else:
-            LastChange=CHANGE
-            #print(LastChange,"returning Change",CHANGE)
+        try:
+            CHANGE=(json.loads(r.text.strip()))
+        except ValueError as e:
             return [False,LastChange]
+        else:
+            if LastChange == CHANGE :
+                #print(LastChange,"returning NoChange",CHANGE)
+                return [True,LastChange]
+            else:
+                LastChange=CHANGE
+                #print(LastChange,"returning Change",CHANGE)
+                return [False,LastChange]
 
 def Update(MYUSR,MYPASS):
     try : 
         r=requests.post(url, data={'USERNAME' : MYUSR,'PASSWORD' : MYPASS, 'GPIO' :'ALL'})
     except requests.ConnectionError as e:
-        return 'UPDATE#ERR'
+        return 'UPDATE*ERR'
     else:
-        Save=(json.loads(r.text.strip()))
-        for i in Save:
-            pin=int(re.sub("[^0-9]", "",i))            
-            state=int(Save[i])
-            
-            #print(pin,type(pin),state,type(state))
-            PiUpdate(pin,state)
-            
-    
+        try:
+            Save=(json.loads(r.text.strip()))
+        except ValueError as e:
+            time.sleep(1)
+        else:        
+            for i in Save:
+                pin=int(re.sub("[^0-9]", "",i))            
+                state=int(Save[i])            
+                #print(pin,type(pin),state,type(state))
+                PiUpdate(pin,state)
 
         
 LastChange=0      
@@ -63,14 +73,16 @@ while True:
  
     if (var[0] == False):        
         Update(MYUSR,MYPASS)
-        #print("Updated at : ",datetime.datetime.now().time())
+        #print("Updated at : ",datetime.datetime.now().time())        
+ 
     elif (var[0] == True):
-        time.sleep(0.5)
+        time.sleep(0.1)
         #print("Rechecking")        
-    elif (var[0] == 'CHANGE#ERR'):
-        #print('CHANGE ERROR')
-        time.sleep(0.5)
-    else :
-        #print("UNKNOWN ERROR")
-        time.sleep(0.5)
+    elif (var[0] == 'CHANGE*ERR'):
+        time.sleep(0.01)
+    else:
+        time.sleep(0.01)
+       
+
+
 
